bd8e53c12112f7cb807efec3a3cdfb4310dae9eb
BeamjunCho9 <107917980+BeamjunCho9@users.noreply.github.com>
2023-03-08 16:38:17 +0900

    입력 받은 두 개의 수를 더하여 결과를 출력하는 방식으로 변경

--- app/src/main/java/com/example/myapplication/controller/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/controller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.myapplication.controller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import com.example.myapplication.R
 import com.example.myapplication.model.Data
@@ -14,12 +15,21 @@ class MainActivity : AppCompatActivity() {
 
         val data = Data()
 
+        val edt1 = findViewById<EditText>(R.id.edt1)
+        val edt2 = findViewById<EditText>(R.id.edt2)
         val text1 = findViewById<TextView>(R.id.text1)
         val btn1 = findViewById<Button>(R.id.btn1)
 
         // 사용자로부터 입력을 받으면 그에 맞는 처리를 진행
         btn1.setOnClickListener {
-            text1.text = "${data.data}"
+            val data = Data()
+            edt1.text?.let {
+                data.num1 = it.toString().toInt()
+            }
+            edt2.text?.let {
+                data.num2 = it.toString().toInt()
+            }
+            text1.text = "${data.calculation()}"
         }
 
     }
--- app/src/main/java/com/example/myapplication/model/Data.kt
+++ app/src/main/java/com/example/myapplication/model/Data.kt
@@ -1,5 +1,10 @@
 package com.example.myapplication.model
 
-data class Data(
-    val data: String = "Model 데이터"
-)
+class Data() {
+    var num1: Int = 0
+    var num2: Int = 0
+
+    fun calculation(): Int {
+        return num1 + num2
+    }
+}
